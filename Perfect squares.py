fc94a522ca1eb7596f7f1d43ac8c7cf751acbafa
Saumen <saumenroy323@gmail.com>
2019-07-31 17:03:25 +0600

    Add files via upload

---
+++ Perfect squares.py
@@ -0,0 +1,14 @@
+def count_perfect(a, b):
+    cnt = 0  # initialize count
+    for i in range(a, b + 1):
+        j = 1
+        while j * j <= i:
+            if j * j == i:
+                cnt = cnt + 1
+
+            j = j + 1
+
+        i = i + 1
+
+    return cnt          
+        
